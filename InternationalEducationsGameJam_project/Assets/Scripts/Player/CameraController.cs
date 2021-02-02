01f9ed433c6b78ac4aef03dbb96c0c88074990db
ReeVrB <78420365+ReeVrB@users.noreply.github.com>
2021-02-02 16:06:55 +0100

    Menno
    Camera Controller, player movement, player healt

---
+++ InternationalEducationsGameJam_project/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //GameObject
+    [SerializeField] private GameObject player;
+
+    //Float
+    [SerializeField] private float smoothSpeed = 0.125f;
+
+    //Vector
+    [SerializeField] private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPos = player.transform.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothedPos;
+    }
+
+}
