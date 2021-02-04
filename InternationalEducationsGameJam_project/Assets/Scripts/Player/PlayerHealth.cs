c7a274126c3f26d41227d94441f72ef7be8357dd
TosaLipkiss <tovahl2002@skola.goteborg.se>
2021-02-04 12:32:35 +0100

    victory screen WIP

--- InternationalEducationsGameJam_project/Assets/Scripts/Player/PlayerHealth.cs
+++ InternationalEducationsGameJam_project/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour, IAttackable
 {
+    public GameObject gameOverScreen;
     //ScriptableObject
     public ScribtablePlayer m_publicplayer;
     public ScribtablePlayer m_playerstats; //editable stats <--
 
     private void Awake()
     {
+        Time.timeScale = 1;
         m_playerstats = Instantiate(m_publicplayer);
     }
 
@@ -18,6 +21,8 @@ public class PlayerHealth : MonoBehaviour, IAttackable
     {
         if (m_playerstats.m_Health - Damage <= 0)
         {
+            Time.timeScale = 0;
+            gameOverScreen.SetActive(true);
             StartCoroutine(Death());
         }
         else
